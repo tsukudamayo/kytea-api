4693d1b0a3717d9ab654230bbcdcbf5ea0f17cc8
tsukuda <v_mayo.tukuda@pactera.com>
2019-10-30 14:00:42 +0900

    corresponding linux 1

--- nerpreprocess.py
+++ nerpreprocess.py
@@ -175,7 +175,7 @@ def insert_space_between_words(text: str) -> str:
     if len(text) == 1:
         pass
     else:
-        text = text.replace('\n', '')
+        # text = text.replace('\n', '')
         words = [w.split('/')[0] if w.count('/') <= 2 else '/' for w in text.split(' ')]
         output = ' '.join(words) + '\n'
 
